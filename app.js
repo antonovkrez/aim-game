2dea45de0e9431e67b622119fd194a46cd2e6c47
Ivan <antonovdev88@gmail.com>
2021-06-20 17:12:39 +0300

    init: add index.html, app.js, styles.css and create new project

---
+++ app.js
@@ -0,0 +1,101 @@
+const startBtn = document.querySelector('#start')
+const screens = document.querySelectorAll('.screen')
+const timeList = document.querySelector('#time-list')
+const timeEl = document.querySelector('#time')
+const board = document.querySelector('#board')
+const colors = ['#eb2315', '#1c15d4', '#d514d9', '#33d911', '#ebc107', '#eb6607']
+const psyMode = document.querySelector('input[type=checkbox]')
+let time = 0
+let score = 0
+
+startBtn.addEventListener('click', (e) => {
+    e.preventDefault()
+    screens[0].classList.add('up')
+})
+
+timeList.addEventListener('click', e => {
+    if (e.target.classList.contains('time-btn')) {
+        time = parseInt(e.target.getAttribute('data-time'))
+        screens[1].classList.add('up')
+        startGame()
+    }
+})
+
+board.addEventListener('click', e => {
+    if (e.target.classList.contains('circle')) {
+        score++
+        e.target.remove()
+        createRandomCircle()
+    } 
+})
+
+function startGame() {
+    setInterval(decreaseTime, 1000)
+    createRandomCircle()
+    setTime(time)
+}
+
+function decreaseTime() {
+    if (time === 0) {
+        finishGame()
+    } else {
+        let current = --time
+        if (current < 10) {
+            current = `0${current}`
+        }
+        setTime(current)
+    }
+}
+
+function setTime(value) {
+    timeEl.innerHTML = `00:${value}`
+}
+
+function finishGame() {
+    timeEl.parentNode.classList.add('hide')
+    board.innerHTML = `<h1 class="end-game">Score: <span class="primary">${score}</span></h1>`
+}
+
+function createRandomCircle() {
+    const circle = document.createElement('div')
+    const size = getRandomNumber(10, 60)
+    const {width, height} = board.getBoundingClientRect()
+    const x = getRandomNumber(0, width - size)
+    const y = getRandomNumber(0, height - size)
+    const color = getRandomColor()
+
+    circle.classList.add('circle')
+    circle.style.width = `${size}px`
+    circle.style.height = `${size}px`
+    circle.style.top = `${y}px`
+    circle.style.left = `${x}px`
+    circle.style.backgroundColor = color
+    circle.style.boxShadow = `0 0 2px ${color}, 0 0 10 px ${color}`
+    if (psyMode.checked) {
+        screens[2].style.background = color
+
+    }
+    
+
+    board.append(circle)
+}
+
+function getRandomNumber(min, max) {
+    return Math.round((Math.random() * (max - min) + min))
+}
+
+function getRandomColor() {
+    const index = Math.floor(Math.random() * colors.length)
+    return colors[index]
+}
+
+function cheatTheGame() {
+    function killCircle () {
+        const circle = document.querySelector('.circle')
+        if (circle) {
+            circle.click()
+        }
+    }
+
+    setInterval(killCircle, 21)
+}
